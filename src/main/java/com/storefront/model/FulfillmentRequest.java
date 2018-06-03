4c0638e05e49b946979e36d4fcdc4a6fcf4d5415
Gary A. Stafford <garystafford@rochester.rr.com>
2018-06-03 16:37:08 -0400

    Fix messaging, refactor entities

--- src/main/java/com/storefront/model/FulfillmentRequest.java
+++ src/main/java/com/storefront/model/FulfillmentRequest.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import org.springframework.data.annotation.Id;
 
 @Data
 @NoArgsConstructor
