e309b78f2793e55e3e4bed9a7657a85eed417bd3
Gary A. Stafford <garystafford@rochester.rr.com>
2018-12-18 21:55:52 -0500

    Add gke branch

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-orders:latest .
-docker push garystafford/storefront-orders:latest
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-orders:gke-2.0.0 .
+docker push garystafford/storefront-orders:gke-2.0.0
 
-# docker run --name storefront-orders -d garystafford/storefront-orders:latest
+# docker run --name storefront-orders -d garystafford/storefront-orders:gke-2.0.0
