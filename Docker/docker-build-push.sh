54c2a2c80fe88547e5610ae5a069edd6981d2d1a
Gary A. Stafford <garystafford@rochester.rr.com>
2021-05-11 17:48:51 -0400

    Updating for 2021.05 release

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -2,8 +2,10 @@
 
 # Usage (from root of project): sh ./Docker/docker-build-push.sh
 
-./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-orders:gke-3.0.0 .
-docker push garystafford/storefront-orders:gke-3.0.0
+# docker build -f Docker/Dockerfile_base --no-cache -t garystafford/storefront-base:3.0.0 .
 
-# docker run --name storefront-orders -d garystafford/storefront-orders:gke-3.0.0
+./gradlew clean build
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-orders:3.0.0 .
+docker push garystafford/storefront-orders:3.0.0
+
+# docker run --name storefront-accounts -d garystafford/storefront-orders:3.0.0
